a18d8353222d1ce4fe0359d2cab91ce4a6e82109
hatien <hatiennguyen39@gmail.com>
2023-05-23 17:44:50 +0700

    push latest code

--- app/src/main/java/com/example/plant/FirstFragment.kt
+++ app/src/main/java/com/example/plant/FirstFragment.kt
@@ -7,24 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.plant.databinding.FragmentFirstBinding
-
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private lateinit var binding: FragmentFirstBinding
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -39,6 +36,5 @@ class FirstFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
